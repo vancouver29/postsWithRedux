255db89eca171033049374a9d26f6ab96119227d
vancouver29 <vancouver2990@gmail.com>
2020-11-02 20:52:25 +0100

    added Directories

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// External imports
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Local imports
+import App from ".App";
+import rootReducer from "./reducers";
