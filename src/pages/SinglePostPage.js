62c696f5d380688c96ffbe3c5b80d9deafd05dc0
vancouver29 <vancouver2990@gmail.com>
2020-11-05 09:51:10 +0100

    added SinglePost and Comments for each single Post

--- src/pages/SinglePostPage.js
+++ src/pages/SinglePostPage.js
@@ -2,35 +2,59 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import { fetchPost } from "../actions/postActions";
+import { fetchComments } from "../actions/commentsActions";
 
 import { Post } from "../components/Post";
+import { Comment } from "../components/Comment";
 
-const SinglePostPage = ({ match, dispatch, post, hasErrors, loading }) => {
+const SinglePostPage = ({
+  match,
+  dispatch,
+  post,
+  hasErrors,
+  loading,
+  comments,
+}) => {
   useEffect(() => {
     const { id } = match.params;
 
     dispatch(fetchPost(id));
+    dispatch(fetchComments(id));
   }, [dispatch, match]);
 
   const renderPost = () => {
+    console.log("loading" + loading);
     if (loading.post) return <p>Loading post...</p>;
     if (hasErrors.post) return <p>Unable to display post.</p>;
 
     return <Post post={post} />;
   };
 
+  const renderComments = () => {
+    console.log("comments: " + comments);
+    if (loading.comments) return <p>Loading comments...</p>;
+    if (hasErrors.comments) return <p>Unable to display post.</p>;
+
+    return comments.map((comment) => (
+      <Comment key={comment.id} comment={comment} />
+    ));
+  };
+
   return (
     <section>
       <h1>Post</h1>
       {renderPost()}
+      <h2>Comments</h2>
+      {renderComments()}
     </section>
   );
 };
 
 const mapStateToProps = (state) => ({
   post: state.post.post,
-  loading: { post: state.post.loading },
-  hasErrors: { post: state.post.hasErrors },
+  loading: { post: state.post.loading, comments: state.comments.loading },
+  hasErrors: { post: state.post.hasErrors, comments: state.comments.hasErrors },
+  comments: state.comments.comments,
 });
 
 export default connect(mapStateToProps)(SinglePostPage);
